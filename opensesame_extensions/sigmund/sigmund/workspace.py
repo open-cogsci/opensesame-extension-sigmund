65ff2d2d934072a3249e8513e2ec451208c33bab
smathot <s.mathot@cogsci.nl>
2025-02-02 15:47:26 +0100

    Add instructions to workspace

--- opensesame_extensions/sigmund/sigmund/workspace.py
+++ opensesame_extensions/sigmund/sigmund/workspace.py
@@ -8,12 +8,14 @@ class WorkspaceManager(BaseWidget):
     def get(self, item_name):
         if item_name not in self.item_store:
             self._item = None
-            self._content = self.experiment.to_string()
-            self._language = 'opensesame'
+            self._content, self._language = self._prepare_general_script()
         else:
             self._item = self.item_store[item_name]
             if self._item.item_type == 'inline_script':
                 self._content, self._language = self._prepare_inline_script()
+            if self._item.item_type == 'inline_javascript':
+                self._content, self._language = \
+                    self._prepare_inline_javascript()
             else:
                 self._content, self._language = self._prepare_item_script()
         return self._content, self._language
@@ -23,28 +25,40 @@ class WorkspaceManager(BaseWidget):
         # previous message, don't do anything
         if not content or content == self._content:
             return
-        # If the workspace content isn't tied to an item, show the general
-        # script
         if self._item is None:
-            self.main_window.regenerate(content)
+            self._parse_general_script(content)
             return
-        # Otherwise open the item in script view
         if self._item.item_type == 'inline_script':
             self._parse_inline_script(content)
-        else:
-            self._parse_item_script(content)
+            return
+        if self._item.item_type == 'inline_javascript':
+            self._parse_inline_javascript(content)
+            return
+        self._parse_item_script(content)
+        
+    def _parse_general_script(self, content):
+        self.main_window.regenerate(content)
             
+    def _prepare_general_script(self):
+        script = f'''# Important instructions: You are now editing the script of an OpenSesame experiment. The scripting language is OpenSesame script, a domain-specific language, and not Python or JavaScript (although Python and JavaScript may be embedded). You can use f-string syntax to include variables and Python expressions, like this: some_keyword="Some value with a {{variable_or_expression}}". Only update the workspace with modifications of this script. Do not put any other content into the workspace. Do not include this instruction comment in your reply.
+{self.experiment.to_string()}
+'''
+        return script, 'opensesame'
+        
     def _parse_item_script(self, content):
         self._item.from_string(content)
         self._item.update()
         self._item.open_tab()
 
     def _prepare_item_script(self):
-        script = self._item.to_string()
         # Normally, the script starts with a 'define' line and is indented by
         # a tab. We want to undo this, and present only unindented content.
-        script = script[script.find(u'\t'):]
-        return textwrap.dedent(script), 'opensesame'
+        script = self._item.to_string()
+        script = textwrap.dedent(script[script.find(u'\t'):])
+        script = f'''# Important instructions: You are now editing the script of an OpenSesame item called {self._item.name} of type {self._item.item_type}. The scripting language is OpenSesame script (and not Python or JavaScript), a domain-specific language. You can use f-string syntax to include variables and Python expressions, like this: some_keyword="Some value with a {{variable_or_expression}}". Only update the workspace with modifications of this script. Do not put any other content into the workspace. Do not include this instruction comment in your reply.
+{script}
+'''
+        return script, 'opensesame'
         
     def _parse_inline_script(self, content):
         pattern = r"# START_PREPARE_PHASE\s*(.*?)\s*# START_RUN_PHASE\s*(.*)"
@@ -59,9 +73,29 @@ class WorkspaceManager(BaseWidget):
         self._item.open_tab()
 
     def _prepare_inline_script(self):
-        return f'''# Important: Preserve the START_PREPARE_PHASE and START_RUN_PHASE markers in your reply.
+        return f'''# Important instructions: You are now editing a Python inline_script item called {self._item.name}. Only update the workspace with modifications of this script. Do not put any other content into the workspace. Do not include this instruction comment in your reply. Do include the START_PREPARE_PHASE and START_RUN_PHASE markers in your reply.
 # START_PREPARE_PHASE
 {self._item.var._prepare}
 # START_RUN_PHASE
 {self._item.var._run}
 ''', 'python'
+
+    def _parse_inline_javascript(self, content):
+        pattern = r"// START_PREPARE_PHASE\s*(.*?)\s*// START_RUN_PHASE\s*(.*)"
+        match = re.search(pattern, content, re.DOTALL)
+        if not match:
+            return
+        prepare = match.group(1).strip()
+        run = match.group(2).strip()
+        self._item.var._prepare = prepare
+        self._item.var._run = run
+        self._item.update()
+        self._item.open_tab()
+
+    def _prepare_inline_javascript(self):
+        return f'''// Important instructions: You are now editing a JavaScript inline_javascript item called {self._item.name}. Only update the workspace with modifications of this script. Do not put any other content into the workspace. Do not include this instruction comment in your reply. Do include the START_PREPARE_PHASE and START_RUN_PHASE markers in your reply.
+// START_PREPARE_PHASE
+{self._item.var._prepare}
+// START_RUN_PHASE
+{self._item.var._run}
+''', 'javascript'
